26ab321f584a44526f796d202ef3bad9aae40eef
RafaelMatta <61514052+RafaelMatta@users.noreply.github.com>
2024-03-25 19:38:12 -0300

    Refactoring the project to the MVC architecture (#5)
    * Changing file structure and fixing the code.
    
    * Refactoring the event handler.

---
+++ src/js/model.js
@@ -0,0 +1,26 @@
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
+
+export const state = {
+    recipe: {},
+}
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await getJSON(`${API_URL}/${id}`)
+
+        const { recipe } = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            ingredients: recipe.ingredients,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+        }
+    } catch (err) {
+        alert(err);
+    }
+}
